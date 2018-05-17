90d55305f9818bbda3957878ae1eaf32ab62639d
Vladimir Rodchenko <vladimir@aurochses.com>
2018-05-17 13:58:30 +0300

    solve translation

--- projects/aurochses/angular-table/src/lib/table.module.ts
+++ projects/aurochses/angular-table/src/lib/table.module.ts
@@ -6,11 +6,12 @@ import { MatTableModule } from '@angular/material/table';
 import { MatIconModule} from '@angular/material/icon';
 import { MatButtonModule } from '@angular/material/button';
 import { MatDialogModule } from '@angular/material/dialog';
-import {MatProgressSpinnerModule} from '@angular/material/progress-spinner';
+import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
+
+import { TranslateModule } from '@ngx-translate/core';
 
 import { TableComponent } from './table.component';
 import { DeleteDialogComponent } from './delete-dialog/delete-dialog.component';
-import {TranslateModule} from '@ngx-translate/core';
 
 @NgModule({
   imports: [
