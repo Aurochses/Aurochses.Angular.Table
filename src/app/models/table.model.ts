a665e7a56a106bb7b969d61aed1558b47808204b
Vladimir Rodchenko <vladimir@aurochses.com>
2018-06-07 17:07:46 +0300

    table select option added

--- src/app/models/table.model.ts
+++ src/app/models/table.model.ts
@@ -1,5 +1,6 @@
-import { Actions, Display, Hidden } from '@aurochses/angular-table';
+import { Select, Actions, Display, Hidden } from '@aurochses/angular-table';
 
+@Select(false)
 @Actions()
 export class TableModel {
     @Display('TABLE.ID_COLUMN')
